e1a846dfced8e1f4c360acc3a90518864746d1e7
JackyChou <913466287@qq.com>
2019-12-11 17:40:20 +0800

    1

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -4,9 +4,7 @@ export default class Loading extends Component {
   render() {
     return (
       <Spin spinning={true}>
-        <div style={{ width: '100%', height: '300px' }}>
-
-        </div>
+        <div style={{ width: '100%', height: '300px' }}></div>
       </Spin>
     )
   }
