02f583601241907ebf0f0acb7add67df32f05462
JackyChou <913466287@qq.com>
2019-12-11 17:39:12 +0800

    载入界面优化

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-
+import { Spin } from 'antd'
 export default class Loading extends Component {
   render() {
     return (
-      <div>
-        Loading……
-      </div>
+      <Spin spinning={true}>
+        <div style={{ width: '100%', height: '300px' }}>
+
+        </div>
+      </Spin>
     )
   }
 }
